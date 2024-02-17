29089d97292be691d2f6bc02b29dc607cad65b21
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-02-17 09:48:08 +0100

    calculate bmi

--- components/common/NumericalTextField/index.tsx
+++ components/common/NumericalTextField/index.tsx
@@ -2,17 +2,26 @@ import React from "react"
 import { Label } from "../Label"
 import { Input } from "../Input"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+interface Props {
+  label?: string
   unit: string
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const NumericalTextField: React.FC<InputProps> = ({ unit, ...props }) => {
+const NumericalTextField: React.FC<Props> = ({
+  label = "",
+  unit,
+  ...props
+}) => {
   return (
     <div>
+      <Label className="text-sm font-normal leading-6 text-dark-electric-blue">
+        {label}
+      </Label>
       <div className="relative">
-        <Input type="text" className="pr-16" {...props} />
-        <span className="text-blue absolute right-5 top-5 text-2xl font-semibold -tracking-[0.075rem]">
+        <Input className="pr-16" {...props} />
+        <span className="absolute right-5 top-5 text-2xl font-semibold -tracking-[0.075rem] text-blue">
           {unit}
         </span>
       </div>
--- components/sections/CalculatorCard.tsx/index.tsx
+++ components/sections/CalculatorCard.tsx/index.tsx
@@ -1,202 +1,171 @@
-"use client"
-
+import { useState, useEffect, useCallback } from "react"
+import { Label } from "../../common/Label"
 import NumericalTextField from "../../common/NumericalTextField"
 import { RadioGroup, RadioGroupItem } from "../../common/RadioGroup"
 import Heading from "../../common/Typography/Heading"
 import Paragraph from "../../common/Typography/Paragraph"
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "../../common/Form"
-import { Button } from "../../common/Button"
-
-const formSchema = z.object({
-  measurementUnit: z.enum(["metric", "imperial"], {
-    required_error: "You need to select a measurement unit.",
-  }),
-  metricHeight: z.number().min(0, {
-    message: "Height must be a positive number.",
-  }),
-  metricWeight: z.number().min(0, {
-    message: "Weight must be a positive number.",
-  }),
-  imperialHeightFt: z.number().min(0, {
-    message: "Height must be a positive number.",
-  }),
-  imperialHeightIn: z.number().min(0, {
-    message: "Height must be a positive number.",
-  }),
-  imperialWeightSt: z.number().min(0, {
-    message: "Weight must be a positive number.",
-  }),
-  imperialWeightLbs: z.number().min(0, {
-    message: "Weight must be a positive number.",
-  }),
-})
-
-type FormSchema = z.infer<typeof formSchema>
 
 const CalculatorCard = () => {
-  const form = useForm<FormSchema>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      measurementUnit: "metric",
-    },
-  })
+  const [unit, setUnit] = useState("metric")
+  // Metric system
+  const [heightCm, setHeightCm] = useState("")
+  const [weightKg, setWeightKg] = useState("")
+  // Imperial system
+  const [feet, setFeet] = useState("")
+  const [inches, setInches] = useState("")
+  const [stones, setStones] = useState("")
+  const [pounds, setPounds] = useState("")
 
-  function onSubmit(values: FormSchema) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  const [bmi, setBmi] = useState<string | null>(null)
+  const [bmiCategory, setBmiCategory] = useState("")
+  const [idealWeight, setIdealWeight] = useState("")
+
+  const calculateBMI = useCallback(() => {
+    let heightInMeters, weightInKg
+
+    if (unit === "metric") {
+      heightInMeters = parseFloat(heightCm) / 100
+      weightInKg = parseFloat(weightKg)
+    } else {
+      const totalInches = parseFloat(feet) * 12 + parseFloat(inches)
+      const totalPounds = parseFloat(stones) * 14 + parseFloat(pounds)
+      heightInMeters = totalInches * 0.0254
+      weightInKg = totalPounds * 0.453592
+    }
+
+    if (heightInMeters > 0 && weightInKg > 0) {
+      const bmiValue = weightInKg / heightInMeters ** 2
+      setBmi(bmiValue.toFixed(1))
+      categorizeBMI(bmiValue)
+      calculateIdealWeight(heightInMeters)
+    } else {
+      setBmi(null)
+      setBmiCategory("")
+      setIdealWeight("")
+    }
+  }, [feet, heightCm, inches, pounds, stones, unit, weightKg])
+
+  const categorizeBMI = (bmiValue: number) => {
+    if (bmiValue < 18.5) {
+      setBmiCategory("Underweight")
+    } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+      setBmiCategory("Healthy weight")
+    } else if (bmiValue >= 25 && bmiValue <= 29.9) {
+      setBmiCategory("Overweight")
+    } else {
+      setBmiCategory("Obesity")
+    }
   }
 
+  const calculateIdealWeight = (heightInMeters: number) => {
+    // Using the Devine Formula for calculating ideal body weight
+    // For men: 50 + 2.3 kg per inch over 5 feet
+    // For women or a general approach: 45.5 + 2.3 kg per inch over 5 feet
+    // Note: Adjust the base weight and formula as per your target audience or specific requirements
+    const idealWeightKg = 45.5 + 2.3 * (heightInMeters * 39.3701 - 60)
+    if (idealWeightKg < 0) return setIdealWeight("")
+    const idealWeightMin = idealWeightKg * 0.9 // Lower range (-10%)
+    const idealWeightMax = idealWeightKg * 1.1 // Upper range (+10%)
+    setIdealWeight(
+      `${idealWeightMin.toFixed(1)}kg - ${idealWeightMax.toFixed(1)}kg`,
+    )
+  }
+
+  useEffect(() => {
+    calculateBMI()
+  }, [unit, heightCm, weightKg, feet, inches, stones, pounds, calculateBMI]) // Recalculate BMI when any input changes
+
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-6 rounded-2xl p-6 shadow-[16px_32px_56px_0_rgba(143,174,207,0.25)] sm:gap-8 lg:max-w-[32.25rem]"
-      >
-        <Heading level={2} size="m">
-          Enter your details below
-        </Heading>
+    <div className="flex flex-col gap-6 rounded-2xl p-6 shadow-[16px_32px_56px_0_rgba(143,174,207,0.25)] sm:gap-8 lg:max-w-[32.25rem]">
+      <Heading level={2} size="m">
+        Enter your details below
+      </Heading>
 
-        <FormField
-          control={form.control}
-          name="measurementUnit"
-          render={({ field }) => (
-            <FormItem className="space-y-3">
-              <FormControl>
-                <RadioGroup
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                  className="grid grid-cols-2 gap-6"
-                >
-                  <FormItem className="flex items-center gap-2 space-x-2 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="metric" />
-                    </FormControl>
-                    <FormLabel className="">Metric</FormLabel>
-                  </FormItem>
-                  <FormItem className="flex items-center space-x-2 space-y-0">
-                    <FormControl>
-                      <RadioGroupItem value="imperial" />
-                    </FormControl>
-                    <FormLabel className="">Imperial</FormLabel>
-                  </FormItem>
-                </RadioGroup>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <RadioGroup className="grid grid-cols-2 gap-6">
+        <div className="flex items-center gap-2">
+          <RadioGroupItem
+            value="metric"
+            id="metric"
+            onClick={() => setUnit("metric")}
+            checked={unit === "metric"}
+          />
+          <Label htmlFor="metric">Metric</Label>
+        </div>
+        <div className="flex items-center gap-2">
+          <RadioGroupItem
+            value="imperial"
+            id="imperial"
+            onClick={() => setUnit("imperial")}
+            checked={unit === "imperial"}
+          />
+          <Label htmlFor="imperial">Imperial</Label>
+        </div>
+      </RadioGroup>
 
+      {unit === "metric" ? (
         <div className="grid gap-6 sm:grid-cols-2">
-          <FormField
-            control={form.control}
-            name="metricHeight"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Height</FormLabel>
-                <FormControl>
-                  <NumericalTextField unit="cm" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+          <NumericalTextField
+            unit="cm"
+            label="Height"
+            value={heightCm}
+            onChange={(e) => setHeightCm(e.target.value)}
           />
-          <FormField
-            control={form.control}
-            name="metricWeight"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Weight</FormLabel>
-                <FormControl>
-                  <NumericalTextField unit="kg" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+          <NumericalTextField
+            unit="kg"
+            label="Weight"
+            value={weightKg}
+            onChange={(e) => setWeightKg(e.target.value)}
           />
         </div>
+      ) : (
         <div className="grid grid-cols-2 place-items-end gap-6">
-          <FormField
-            control={form.control}
-            name="imperialHeightFt"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Height</FormLabel>
-                <FormControl>
-                  <NumericalTextField unit="ft" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+          <NumericalTextField
+            unit="ft"
+            label="Height"
+            value={feet}
+            onChange={(e) => setFeet(e.target.value)}
           />
-          <FormField
-            control={form.control}
-            name="imperialHeightIn"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <NumericalTextField unit="in" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+          <NumericalTextField
+            unit="in"
+            value={inches}
+            onChange={(e) => setInches(e.target.value)}
           />
-          <FormField
-            control={form.control}
-            name="imperialWeightSt"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Weight</FormLabel>
-                <FormControl>
-                  <NumericalTextField unit="st" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+          <NumericalTextField
+            unit="st"
+            label="Weight"
+            value={stones}
+            onChange={(e) => setStones(e.target.value)}
           />
-          <FormField
-            control={form.control}
-            name="imperialWeightLbs"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <NumericalTextField unit="lbs" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+          <NumericalTextField
+            unit="lbs"
+            value={pounds}
+            onChange={(e) => setPounds(e.target.value)}
           />
         </div>
-        <div className="bg-blue flex flex-col gap-6 rounded-2xl p-8 text-white sm:flex-row sm:items-center sm:rounded-r-[12rem]">
-          <div>
-            <Paragraph size="m-bold" className="text-white">
-              Your BMI is...
-            </Paragraph>
-            <Heading level={3} size="xl">
-              23.4
-            </Heading>
-          </div>
-          <Paragraph className="ml-auto max-w-[17rem] text-white">
-            Your BMI suggests you’re a healthy weight. Your ideal weight is
-            between <span className="font-bold">63.3kgs - 85.2kgs</span>.
+      )}
+
+      <div className="flex flex-col gap-6 rounded-2xl bg-blue p-8 text-white sm:flex-row sm:items-center sm:rounded-r-[12rem]">
+        <div>
+          <Paragraph size="m-bold" className="text-white">
+            Your BMI is...
           </Paragraph>
+          <Heading level={3} size="xl">
+            {bmi || "N/A"}
+          </Heading>
+          <Paragraph className="text-white">{bmiCategory}</Paragraph>
         </div>
-        <Button type="submit" className="bg-blue text-white">
-          Sumbit
-        </Button>
-      </form>
-    </Form>
+        <Paragraph className="ml-auto max-w-[17rem] text-white">
+          {bmiCategory === "Healthy weight"
+            ? "You have a healthy weight. "
+            : ""}
+          {!bmi || Number(bmi) < 0
+            ? "Enter your details to calculate your BMI and ideal weight."
+            : idealWeight
+              ? `Your ideal weight is between ${idealWeight}.`
+              : ""}
+        </Paragraph>
+      </div>
+    </div>
   )
 }
 
